da34fb651c7c778e8d100ab0bc93a9e46ca90b7d
V0r-T3x <70115207+V0r-T3x@users.noreply.github.com>
2020-08-23 23:44:44 -0400

    CMSTest.js
    a HIDScript for P4wnP1 aloa to test the layout of a Canadian Multilingual Standard (CMS) keyboard layout.

---
+++ CMSTest.js
@@ -0,0 +1,57 @@
+layout("CMS");
+typingSpeed(100,100);
+press("GUI r");
+delay(500);
+type("notepad\n");
+delay(500);
+typingSpeed(0,0);
+//to open in a textpad
+press("CTRL a");	//Select all
+press("DEL");		//Delete all
+
+type("//No modifier\n");
+type("//SHIFT\n");
+type("/1234567890-=\n");
+type("\\!@#$%?&*()_+\n");
+type("qwertyuiop^ç\n");
+type("QWERTYUIOP¨Ç\n");
+type("asdfghjkl;èà\n");
+type("ASDFGHJKL:ÈÀ\n");
+type("zxcvbnm,.é\n");
+type("ZXCVBNM'\"É\n \n \n");
+
+type("//No modifier\n");
+type("//RIGHT_ALT\n");
+type("/1234567890-=\n");
+type("|   ¤  {}[] ¬\n");
+type("qwertyuiop^ç\n");
+type("  €       `~\n");
+type("asdfghjkl;èà\n");
+type("         °  \n");
+type("zxcvbnm,.é\n");
+type("«»     <>\n \n \n");
+
+type("//No modifier\n");
+type("//RIGHT_CTRL\n");
+type("/1234567890-=\n");
+type(" ¹²³¼½¾     ¸\n");
+type("qwertyuiop^çà\n");
+type(" łœ¶ŧ←↓→øþ ~ \n");
+type("asdfghjkl;\è\n");
+type("æßð ŋħĳĸŀ´ \n");
+type("zxcvbnm,.\é\n");
+type("  ¢“”ŉµ―· \n \n \n");
+
+type("//No modifier\n");
+type("//SHIFT + RIGHT_CTRL\n");
+type("/1234567890-=\n");
+type(" ¡ £€⅜⅝⅞™± ¿˛\n");
+type("qwertyuiop^ç\n");
+type("ΩŁŒ®Ŧ¥↑ıØÞ˚¯\n");
+type("asdfghjkl;èà\n");
+type("Æ§ÐªŊĦĲ Ŀ˝ˇ˘\n");
+type("zxcvbnm,.é\n");
+type("  ©‘’♪º×÷·\n \n \n");
+
+type("//Characters with accents\n");
+type("âÂäÄàÀãÃáÁąĄåÅāĀăĂ\néÉêÊëËèÈėĖęĘēĒěĚ\nîÎïÏìÌĩĨíÍįĮīĪ\nôÔöÖòÒõÕóÓōŌőŐ\nûÛüÜùÙũŨųŲůŮūŪűŰŭŬ\nŷŶÿŸýÝýÝ\nŵŴ\nŕŔŗŖřŘ\nţŢťŤ\nŝŜśŚşŞšŠ\nďĎ\nĝĜġĠģĢğĞ\nĥĤ\nĵĴĶ\nĺĹļĻľĽ\nżŻźŹžŽ\nñÑńŃņŅňŇ\nĉĈçÇċĊćĆčČ\n")
